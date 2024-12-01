4d5c173c0759eaaad48a374f2252471731e1ea15
mahdi056 <ahnafmahdi12@gmail.com>
2024-12-02 00:16:57 +0600

    Welcome Page

---
+++ app/src/main/java/com/example/safinalproject/Welcome.java
@@ -0,0 +1,35 @@
+package com.example.safinalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button btnwelcome = findViewById(R.id.btn_welcome);
+
+        btnwelcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Welcome.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+
+    }
+}
